5206e5be883684ab353f46921427a4b3c33b5254
newbe01 <pkwaa3@gmail.com>
2023-07-12 22:04:32 +0900

    price-compare-entity

---
+++ src/main/java/com/example/pricecompareredis/vo/Product.java
@@ -0,0 +1,11 @@
+package com.example.pricecompareredis.vo;
+
+import lombok.Data;
+
+@Data
+public class Product {
+
+    private String productID;   // "000A"
+    private int price;          // 25000
+
+}
