0965a0844cae39dde2b05550148536dcfd2b808d
newbe01 <pkwaa3@gmail.com>
2023-07-13 23:09:50 +0900

    price-compare
    keyword 기준으로 데이터를 가지고 오는 기능

--- src/main/java/com/example/pricecompareredis/service/LowestPriceServiceImpl.java
+++ src/main/java/com/example/pricecompareredis/service/LowestPriceServiceImpl.java
@@ -1,14 +1,14 @@
 package com.example.pricecompareredis.service;
 
+import com.example.pricecompareredis.vo.Keyword;
 import com.example.pricecompareredis.vo.Product;
 import com.example.pricecompareredis.vo.ProductGrp;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 @Service
 public class LowestPriceServiceImpl implements LowestPriceService{
@@ -56,6 +56,55 @@ public class LowestPriceServiceImpl implements LowestPriceService{
         int rank = redisTemplate.opsForZSet().rank(keyword, prodGrpId).intValue();
 
         return rank;
+    }
+
+    @Override
+    public Keyword GetLowestPriceProductByKeyword(String keyword) {
+
+        Keyword returnInfo = new Keyword();
+        List<ProductGrp> tempProdGrp = getProdGrpUsingKeyword(keyword);
+
+        returnInfo.setKeyword(keyword);
+        returnInfo.setProductGrpList(tempProdGrp);
+
+        return returnInfo;
+    }
+
+    public List<ProductGrp> getProdGrpUsingKeyword(String keyword) {
+        List<ProductGrp> returnInfo = new ArrayList<>();
+
+        List<String> prodGrpIdList = new ArrayList<>();
+        prodGrpIdList = List.copyOf(redisTemplate.opsForZSet().reverseRange(keyword, 0, 9));
+        List<Product> tempProdList = new ArrayList<>();
+
+        for (final String prodGrpId : prodGrpIdList) {
+
+            ProductGrp tempProdGrp = new ProductGrp();
+
+            Set prodAndPriceList = new HashSet();
+            prodAndPriceList = redisTemplate.opsForZSet().rangeWithScores(prodGrpId, 0, 9);
+
+            Iterator<Object> prodPriceObj = prodAndPriceList.iterator();
+
+            while (prodPriceObj.hasNext()) {
+                ObjectMapper objectMapper = new ObjectMapper();
+                Map<String, Object> prodPriceMap = objectMapper.convertValue(prodPriceObj.next(), Map.class);
+                Product tempProduct = new Product();
+
+                tempProduct.setProductID(prodPriceMap.get("value").toString());
+                tempProduct.setPrice(Double.valueOf(prodPriceMap.get("score").toString()).intValue());
+                tempProduct.setProdGrpId(prodGrpId);
+
+                tempProdList.add(tempProduct);
+            }
+
+            tempProdGrp.setProdGrpId(prodGrpId);
+            tempProdGrp.setProductList(tempProdList);
+
+            returnInfo.add(tempProdGrp);
+        }
+
+        return returnInfo;
     }
 
 }
